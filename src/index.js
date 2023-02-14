ce3c4c9152917fd59cfa87d3ad8243bd289944bc
Junya Ishida <100757861+jinyaishida@users.noreply.github.com>
2023-02-14 23:27:31 +0900

    2/14分まで

--- src/index.js
+++ src/index.js
@@ -141,3 +141,19 @@
 // const num = null;
 // const fee = num || "金額未設定です";
 // console.log(fee);
+
+import "./styles.css";
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  div.appendChild(li);
+  document.getElementById("imcomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());

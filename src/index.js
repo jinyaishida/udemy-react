2a2bd7bf0c34fcba01cc85083684b43ba60d7dea
Junya Ishida <100757861+jinyaishida@users.noreply.github.com>
2023-02-16 09:25:17 +0900

    削除ボタン表示まで

--- src/index.js
+++ src/index.js
@@ -146,11 +146,23 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+
   const div = document.createElement("div");
   div.className = "list-row";
+
   const li = document.createElement("li");
   li.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
   document.getElementById("imcomplete-list").appendChild(div);
 };
 
